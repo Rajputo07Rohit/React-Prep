8f59bff794b4381442d3ac514a83303a0640c84a
Rohit Singh <rajputrohit2k@gmail.com>
2025-07-28 21:01:50 +0530

    Initial push from Gemini-Clonne

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './components/SIdebar/Sidebar';
+import Main from './components/Mainf/Main';
+
+
+const App = () => {
+  return (
+    <>
+   <Sidebar />
+ <Main />
+    </>
+  )
+}
+
+export default App
